ea40467e2ab8dbe97fefc4957599b9b819e7c790
Aakashmk545 <aakashmk054@gmail.com>
2024-02-09 18:29:25 +0530

    website templete in react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react'
+
+
+// ................CANADA WEBSITE ........
+
+
+import Canada from './components/Canada/HeaderIndex.js';
+function App()
+{
+  return(
+      <Canada/>
+  )
+}
+export default App;
+
+
+
